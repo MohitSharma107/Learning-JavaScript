afe5b1d0dd4057363575940188a4a7b61078db96
Mohit Sharma <msair107@gmail.com>
2018-09-24 15:52:01 +0530

    variable mutation and type coercion

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/*****************************************
+ * varibale and datatypers
+ */
+
+ /*
 console.log('Hello world !!!');
 
 var firstName = 'jhon';
@@ -14,7 +19,38 @@ console.log(job);
 
 job = 'Teacher';
 console.log(job);
+*/
+
+/*****************************************
+ * varibale mutation and type coercion
+ */
+
+ var firstName = 'jhon';
+ var age = 28;
+
+//  Type coercion
+
+ //Type coercion means conversion from one data type to another datatype. here jhon is a string
+ // '' is also a string but age is Number so what will happen here is age which is of Number datatype
+ // will convert into string data type in console.log statement. 
+ console.log(firstName + '' + age);
+
+ var job, isMarried;
+ job = 'teacher';
+ isMarried = false;
+
+ console.log(firstName + ' is a ' + age + ' year old ' + job + ' . is he married? ' + isMarried);
+
+ //Variable Mutation 
+//  it is just same like variable reassignment. like we did here with age
+
+ age = 'twenty eight';
+ job = 'driver';
+
+ alert(firstName + ' is a ' + age + ' year old ' + job + ' . is he married? ' + isMarried);
 
 
+ var lastName = prompt('what is her last name');
+ console.log(firstName + ' ' + lastName);
 
 
