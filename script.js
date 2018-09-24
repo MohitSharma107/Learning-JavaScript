540d554a6b12c15c356a20f0bab939e35e836776
Mohit Sharma <msair107@gmail.com>
2018-09-24 15:02:19 +0530

    variable and datatypes

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('Hello world !!!');
+
+var firstName = 'jhon';
+console.log(firstName);
+
+var lastName = 'smith';
+var age = 28;
+
+var fullAge = 'true';
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+
+
+
