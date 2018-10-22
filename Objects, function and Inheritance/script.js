ed77b69777a6587c66965a0c9f08c533fca28104
Mohit Sharma <msair107@gmail.com>
2018-10-22 17:39:38 +0530

    last commit in objeccts, function and inheritance

--- Objects, function and Inheritance/script.js
+++ Objects, function and Inheritance/script.js
@@ -489,7 +489,7 @@ c) correct answer (I would use a number for this)
 */
 
 
-
+//function constructor prototype
 (function() {
     function Question(question, answers, correct) {
         this.question = question;
